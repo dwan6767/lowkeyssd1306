9ea2bb73b208979796eeb0d3bcb286864cbf26a2
dwan6767 <dwanshikari@gmail.com>
2025-06-23 13:11:02 +0000

    added some bro comments

--- examples/flappy.ino
+++ examples/flappy.ino
@@ -1,3 +1,10 @@
+/*
+  lowkeyoled - Minimal SSD1306 I2C OLED driver for AVR/Arduino
+
+  Written by Dwaipayan Shikari (https://github.com/dwan6767)
+  License: MIT
+*/
+// hardware needed avr microcontroller , ssd1306 oled screen , push button , wires ,debouncing capacitor(100nf) etc
 #include "lowkeyoled.h"
 
 #define horizontal_offset 4
@@ -5,7 +12,7 @@
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
 
-uint16_t pre = 0;
+uint16_t pre = 0;   // i bet no one can play more than one minute all these to save some important RAM bro!you can change it it 32 bit if you are pro
 uint16_t now;
 uint8_t count = 0;
 
@@ -13,20 +20,19 @@ uint8_t count = 0;
 void setup() {
   pinMode(2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), Isr, FALLING);
-
+//using an interrupt for key detection
   oled_clear();
   oled_init();
   oled_clear();
 
-  flappy(3, 120);
+  flappy(3, 120);  //our chill guy
   building1(0);
   oled_update();
 }
-
+// in these code you may be see random number numbers actually i didn't use any maths just some trail and error values 
 void loop() {
   static bool u = false;
   static uint8_t y = 4;
-
   now = millis();
   oled_clear();
 
@@ -58,7 +64,7 @@ void block(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2) {
       oled_draw_pixel(i, j, true);
     }
   }
-}
+} /// a simple block from the draw pixel function 
 
 void flappy(uint8_t i, uint8_t j) {
   oled_draw_pixel(1 + j, 31 + i, true);
@@ -67,7 +73,7 @@ void flappy(uint8_t i, uint8_t j) {
   oled_draw_pixel(2 + j, 28 + i, true);
  oled_draw_pixel(1 + j, 27 + i, true);
 }
-
+// flappy movable low key bird
 void building1(uint8_t i) {
   block(15 + i, 0, 28 + i, 25);
   block(40 + i, 0, 60 + i, 35);
@@ -89,3 +95,5 @@ void building2(uint8_t i) {
   block(72 + i, 31, 85 + i, 64);
   block(97 + i, 52, 115 + i, 64);
 }
+//only using two buildings for the flappy universe switch intermediately
+//most unproductive way to write games change and build on your way
--- src/lowkeyoled.cpp
+++ src/lowkeyoled.cpp
@@ -1,3 +1,10 @@
+/*
+  lowkeyoled - Minimal SSD1306 I2C OLED driver for AVR/Arduino
+
+  Written by Dwaipayan Shikari (https://github.com/dwan6767)
+  License: MIT
+*/
+
 #include "lowkeyoled.h"
 #include<Arduino.h>
 #include <Wire.h>
@@ -5,7 +12,7 @@
 #include <avr/pgmspace.h>
 #include <string.h>
 
-#define H_OFFSET 4
+#define H_OFFSET 4  // came due some faulty hardware may not be in your case so change it by assinging it a different value
 
 static const uint8_t oled_init_cmds[] PROGMEM = {
   0xAE, 0xD5, 0x80, 0xA8, 0x3F,
@@ -13,22 +20,22 @@ static const uint8_t oled_init_cmds[] PROGMEM = {
   0x20, 0x00, 0xA1, 0xC8, 0xDA, 0x12,
   0x81, 0xCF, 0xD9, 0xF1, 0xDB, 0x40,
   0xA4, 0xA6, 0xAF
-};
+};  //These are some commands which help to initialize the display driver , GDDRAM , Charge pump for the led driver ,charge pumps are capacitive circuit which gives boost in voltage to overcome led forward drop , also setting display clock , display on-off ,offsets,contrast control and many more settings more more information refer datasheet 
 
-uint8_t oled_buffer[OLED_WIDTH * OLED_HEIGHT / 8];
+uint8_t oled_buffer[OLED_WIDTH * OLED_HEIGHT / 8];  // Main RAM consumer. there is another way which stores it in progmem , but those are really good for static display , for dynamic update RAM method is faster  
 
 void oled_send_command(uint8_t cmd) {
   Wire.beginTransmission(OLED_ADDR);
-  Wire.write(0x00); // command mode
+  Wire.write(0x00); // this let's driver to read data as commands
   Wire.write(cmd);
   Wire.endTransmission();
 }
 
 void oled_init(void) {
   Wire.begin();
-  _delay_ms(50);
+  _delay_ms(50); //small delay may not be necessary but useful 
   for (uint8_t i = 0; i < sizeof(oled_init_cmds); i++) {
-    uint8_t cmd = pgm_read_byte(&oled_init_cmds[i]);
+    uint8_t cmd = pgm_read_byte(&oled_init_cmds[i]);  // using the avr flash read access
     oled_send_command(cmd);
   }
   oled_clear();
@@ -36,12 +43,12 @@ void oled_init(void) {
 }
 
 void oled_clear(void) {
-  memset(oled_buffer, 0, sizeof(oled_buffer));
+  memset(oled_buffer, 0, sizeof(oled_buffer)); //clear the information from the buffer to all zero 
 }
 
 void oled_update(void) {
   for (uint8_t page = 0; page < 8; page++) {
-    oled_send_command(0xB0 + page);
+    oled_send_command(0xB0 + page);  
     oled_send_command(0x00);
     oled_send_command(0x10);
 
@@ -55,7 +62,7 @@ void oled_update(void) {
     }
   }
 }
-
+//the ssd 1306 driver uses this kind of addressing modes for their data sending , they use 64/8 =8 pages , in this snippet they address pages by these fashion then after addressing , switched to data mode then it will be easy for us to send the data from the buffer , well i2c hardware limitation has 16bytes (2pages fair enough) thus the infromation goes to their RAM 
 void oled_draw_pixel(uint8_t x, uint8_t y, bool color) {
   if (x >= OLED_WIDTH || y >= OLED_HEIGHT) return;
   uint16_t index = x + (y / 8) * OLED_WIDTH + H_OFFSET;
@@ -65,3 +72,5 @@ void oled_draw_pixel(uint8_t x, uint8_t y, bool color) {
   else
     oled_buffer[index] &= ~bit;
 }
+// only added a draw pixel function for minimality , only two possible states true means led on ,false means off , suppose you want to draw pixel at (10,20) then it is in page (20%8) page 2 the position will be (the ram fills in page by page fashion so it must have gone through 10+2*64 =138 pages first before reaching to your desired pixel , then a 1byte bitmasked buffer holds the information of that single page
+ 
